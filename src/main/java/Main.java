cfa70c093f0c571e93a53fb65333f63c4f7e9871
quelbz <quelbyfernandez18@gmail.com>
2024-12-27 20:58:11 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    int i = 5;
+    public static void main(String[] args) {
+        int j = 5;
+        System.out.println(Second.x());
+        System.out.println(Second.x(10,20));
+        Second.gh = 65;
+    }
+
+
+}
---
+++ src/main/java/Second.java
@@ -0,0 +1,13 @@
+public class Second {
+
+    public static int x () {
+        Main newObj = new Main();
+        return newObj.i;
+    }
+
+    public static int x (int y, int z) {
+        return y + z;
+    }
+
+    public static int gh = 22;
+}
